97b3fa7302305538b3583a3d0e59ffda42dd38d4
Uwe Riegel <uriegel@hotmail.de>
2022-04-12 17:19:40 +0200

    QStandardItemModel including QFileSystemModel

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,6 @@
 #include <QFileSystemModel>
+#include <QStandardItemModel>
+#include <QDateTime>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -8,11 +10,36 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    auto model = new QFileSystemModel(this);
+    auto model = new QStandardItemModel();
+    model->setHorizontalHeaderItem(0, new QStandardItem("Subject"));
+    model->setHorizontalHeaderItem(1, new QStandardItem("Größe"));
+    model->setHorizontalHeaderItem(2, new QStandardItem("Datum"));
+
+    auto fileModel = new QFileSystemModel(this);
     auto path = QDir::homePath() + "/Dokumente";
-    model->setRootPath(path);
+    fileModel->setRootPath(path);
+
     ui->folderView->setModel(model);
-    ui->folderView->setRootIndex(model->index(path));
+
+    connect(fileModel, &QFileSystemModel::directoryLoaded, [model, fileModel](const QString &directory) {
+        auto parentIndex = fileModel->index(directory);
+        int numRows = fileModel->rowCount(parentIndex);
+        for (auto i = 0; i < numRows; i++) {
+            auto indexi = fileModel->index(i, 0, parentIndex);
+            auto content = fileModel->data(indexi, Qt::DisplayRole).toString();
+            auto eikon = fileModel->fileIcon(indexi);
+            auto seize = fileModel->size(indexi);
+            auto lm = fileModel->lastModified(indexi);
+
+            auto list = QList<QStandardItem*>();
+            list.append(new QStandardItem(eikon, content));
+            list.append(new QStandardItem(QString::number(seize)));
+            list.append(new QStandardItem(lm.toString()));
+
+            model->appendRow(list);
+        }
+        delete fileModel;
+    });
 }
 
 MainWindow::~MainWindow()
