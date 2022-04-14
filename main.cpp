e9f7917fbdbc693fbf1278c492b965e8975e57a6
Uwe Riegel <uriegel@hotmail.de>
2022-04-14 08:07:09 +0200

    TODOs

--- main.cpp
+++ main.cpp
@@ -9,3 +9,13 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
+// TODO Tab Key process, no emitting -> focus stys in FolderView
+// TODO Extend FilesModels with parent item ..
+// TODO Sorting (string (name), integer (size) date time, but always directory sort by name before files
+// TODO get exifs in a background thread, then change model
+// TODO Sorting by datetime or exiftime
+// TODO change path by enter and double click
+// TODO Edit control to show path and change it -> tab
+// TODO new model: show drives
+
