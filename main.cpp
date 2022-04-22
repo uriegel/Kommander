364e6c222608692597fb70dd60a422c50fdaef10
Uwe Riegel <uriegel@hotmail.de>
2022-04-22 21:16:49 +0200

    Viewer: QStackedWidget, Video, Image

--- main.cpp
+++ main.cpp
@@ -12,8 +12,6 @@ int main(int argc, char *argv[])
 }
 
 // TODO menu show/hide viewer F3
-// TODO Pdfview https://doc.qt.io/qt-5/qtpdf-pdfviewer-example.html at first create sample
-// TODO Media player at first create sample
 // TODO Save window dimensions
 // TODO show hidden semi transparent/filter hidden
 // TODO new model: show drives
